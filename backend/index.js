c5e1d5b435f8f34114c4545625cd3fe735505303
rishabhkukreja30 <rishabhkukreja30@gmail.com>
2024-02-28 23:19:18 +0530

    added routes structuring

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,13 @@
 const express = require("express");
+const mainRouter = require("./routes/index");
 
 const app = express();
 
+const PORT = 3000;
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hello rishbh");
-});
-
-const PORT = 3000;
+app.use("/api/v1", mainRouter);
 
 app.listen(PORT, () => {
   console.log(`Sever started on port ${PORT}`);
